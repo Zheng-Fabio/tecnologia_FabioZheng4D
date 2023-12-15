24e2659845469aabced9ae4a646cae45d4145087
Fabio Zheng <zheng.fabio@iisgalvanimi.edu.it>
2023-12-15 10:46:24 +0000

    Con il nuovo esercizio Es00.1.2

---
+++ Es00.1.2/Esxverifica/src/app/fourth-component/fourth-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fourth-component',
+  templateUrl: './fourth-component.component.html',
+  styleUrls: ['./fourth-component.component.css']
+})
+export class FourthComponentComponent {
+  string ="quarto"
+
+}
---
+++ Es00.1.2/Esxverifica/src/app/second-component/second-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-second-component',
+  templateUrl: './second-component.component.html',
+  styleUrls: ['./second-component.component.css']
+})
+export class SecondComponentComponent {
+  string ="second"
+
+}
